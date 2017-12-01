9380a7ed6715c72ccbc0c7f00d67f40b38f8cb05
Joalbert González <joalbertgonzalez@gmail.com>
2017-12-01 14:04:31 -0500

    add achievement

--- js/overview.js
+++ js/overview.js
@@ -1,3 +1,6 @@
+var TECH = 1800;
+var HSE = 1200;
+
 // inicio de funcionalidad de Enrollment
 
 // obtiene el total de estudiantes
@@ -8,9 +11,9 @@ function getTotalStudents(students) {
 // obtiene el total de deserciones
 function getDropoutStudents(students) {
   var totalDropouts = 0;
-  var studentsLength = getTotalStudents(students);
+  var totalStudents = getTotalStudents(students);
 
-  for (var i = 0; i < studentsLength; i++) {
+  for (var i = 0; i < totalStudents; i++) {
     var student = students[i];
 
     if (!student.active) {
@@ -49,45 +52,89 @@ function enrollment(data, campus = campusDefault, cohort = cohortDefault) {
 // fin de funcionalidad de Enrollment
 
 // inicio de funcionalidad de Achievement
-function getTargetedStudents(students) {
-  var targetedStudents = 0;
-  var studentsLength = getTotalStudents(students);
 
-  for (var i = 0; i < studentsLength; i++) {
+// 70% de la cantidad total de puntos tech
+function metTech() {
+  return TECH * 0.7;
+}
+
+// 70% de la cantidad total de puntos hse
+function metHse() {
+  return HSE * 0.7;
+}
+
+function getTargetedStudents(students) {
+  // estudiantes que superan
+  var targetedStudents = 0;
+  var totalStudents = getTotalStudents(students);
+
+  metTech = metTech();
+  metHse = metHse();
+
+  for (var i = 0; i < totalStudents; i++) {
     var student = students[i];
 
+    // si la estudiante esta activa
     if (student.active) {
       var totalTech = 0, totalHse = 0;
       var sprints = student.sprints;
-      var sprintsLength = sprints.length;
       var totalSprints = sprints.length;
-      var hasMetTechTarget = 0;
-      var hasMetHseTarget = 0;
+      var hasMetTechTarget = false;
+      var hasMetHseTarget = false;
 
-      for (var j = 0; j < sprintsLength; j++) {
+      // se recorren todos sus sprints,
+      // y se suman los puntos tech y hse
+      for (var j = 0; j < totalSprints; j++) {
         var sprint = sprints[j];
 
         totalTech += sprint.score.tech;
         totalHse += sprint.score.hse;
       }
 
-      hasMetTechTarget = (totalTech / totalSprints) > 1260;
-      hasMetHseTarget = (totalHse / totalSprints) > 840;
+      hasMetTechTarget = (totalTech / totalSprints) > metTech;
+      hasMetHseTarget = (totalHse / totalSprints) > metHse;
 
       if (hasMetTechTarget && hasMetHseTarget) {
         targetedStudents++;
       }
     }
   }
+  return targetedStudents;
 }
 
+// porcentaje de estudiantes que cumplen tech y hse
+function getAchievementPercent(students, targetedStudents) {
+  return (targetedStudents / students * 100).toFixed(2);
+}
+
+// obtiene el total de estudiantes activos
+function getActiveStudents(students) {
+  var totalActiveStudents = 0;
+  var totalStudents = getTotalStudents(students);
+
+  for (var i = 0; i < totalStudents; i++) {
+    var student = students[i];
+
+    if (student.active) {
+      totalActiveStudents++;
+    }
+  }
+  return totalActiveStudents;
+}
+
+// crea un objeto para la funcionalidad de achievement
+// con la key targetedStudents, totalStudents y targetedStudentsPercent
 function achievement(data, campus = campusDefault, cohort = cohortDefault) {
   var objAchievement = {};
   var students = getStudents(data, campus, cohort);
 
-  objAchievement.targetedStudents = getTargetedStudents(students);
-  // objAchievement.targetedStudentsPercent = getAchievementPercent(totalStudents, targetedStudents);
-  // objAchievement.totalStudents = getActiveStudents(students);
+  var targetedStudents = getTargetedStudents(students);
+  var totalStudents = getActiveStudents(students);
+  var targetedStudentsPercent = getAchievementPercent(totalStudents, targetedStudents);
+
+  objAchievement.targetedStudents = targetedStudents;
+  objAchievement.totalStudents = getActiveStudents(students);
+  objAchievement.targetedStudentsPercent = targetedStudentsPercent;
 
   return objAchievement;
 }
