32bbf352ae10d3fda8d5f288aa3e9ddae6305c5a
Joalbert González <joalbertgonzalez@gmail.com>
2017-12-03 21:53:30 -0500

    add z-index

--- js/app.js
+++ js/app.js
@@ -50,3 +50,22 @@ function getRatings(data, campus, cohort) {
   return data[campus][cohort].ratings;
 }
 /* var temp = getStudents(data); */
+
+// Funcionalidad del dropdown
+var dropdown = document.getElementsByClassName('dropdown');
+
+for (var i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener('click', showMenu);
+}
+
+function showMenu() {
+  var listMenu = this.getElementsByClassName('item-list')[0];
+
+  if (listMenu.classList.contains('hide')) {
+    listMenu.classList.remove('hide');
+    listMenu.classList.add('show');
+  } else {
+    listMenu.classList.remove('show');
+    listMenu.classList.add('hide');
+  }
+}
