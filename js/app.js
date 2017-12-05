d3f7b3939795996c038e1a85227fdc997b524508
Joalbert González <joalbertgonzalez@gmail.com>
2017-12-04 22:51:13 -0500

    add dropdown and callOverview

--- js/app.js
+++ js/app.js
@@ -12,14 +12,22 @@ var HSE = 1200;
 // se crearan las keys cohorts y cohortsList, las cuales representan
 // el numero de cohortes y una lista de las cohortes respectivamente
 var centers = [
-  {name: 'arequipa',
-    key: 'AQP'},
-  {name: 'ciudad de mexico',
-    key: 'CDMX'},
-  {name: 'lima',
-    key: 'LIM'},
-  {name: 'santiago de chile',
-    key: 'SCL'}
+  {
+    name: 'arequipa',
+    key: 'AQP'
+  },
+  {
+    name: 'ciudad de méxico',
+    key: 'CDMX'
+  },
+  {
+    name: 'lima',
+    key: 'LIM'
+  },
+  {
+    name: 'santiago de chile',
+    key: 'SCL'
+  }
 ];
 
 // se crean la cantidad de cohortes por sede
@@ -35,7 +43,9 @@ function getCohorts() {
   }
 }
 
+// cargamos los keys para identificar los cohorts
 getCohorts();
+
 var ZERO = 0;
 var campusDefault = centers[ZERO].key;
 var cohortDefault = centers[ZERO].cohortsList[ZERO];
@@ -51,26 +61,133 @@ function getRatings(data, campus, cohort) {
 }
 /* var temp = getStudents(data); */
 
+function campusCohortDropdown(campus = centers) {
+  var totalCampus = Object.keys(campus).length;
+  var title = document.getElementsByClassName('cohort')[0].firstElementChild;
+  var dropdown = document.getElementsByClassName('dropdown')[0];
+  var itemList = dropdown.getElementsByClassName('item-list')[0];
+
+  title.textContent = campus[0].name.toUpperCase() + ' ' + campus[0].cohortsList[0];
+  itemList.textContent = '';
+
+  for (var i = 0; i < totalCampus; i++) {
+    var li = document.createElement('li');
+    var span = document.createElement('span');
+    var totalCohort = campus[i].cohorts;
+
+    li.classList.add('line');
+    span.classList.add('checkbox');
+
+    li.appendChild(span);
+    li.appendChild(document.createTextNode(campus[i].name));
+
+    itemList.appendChild(li);
+
+    for (var j = 0; j < totalCohort; j++) {
+      li = document.createElement('li');
+      span = document.createElement('span');
+
+      span.classList.add('checkbox');
+
+      li.appendChild(span);
+      li.appendChild(document.createTextNode(campus[i].cohortsList[j]));
+
+      itemList.appendChild(li);
+    }
+  }
+}
+
+campusCohortDropdown();
+
+// llenar dropdown
 // Funcionalidad del dropdown
 var dropdowns = document.getElementsByClassName('dropdown');
 var totalDropdowns = dropdowns.length;
 
+// se agregan el evento click por cada dropdown
 for (var i = 0; i < totalDropdowns; i++) {
   dropdowns[i].addEventListener('click', showMenu);
 }
 
 function showMenu(event) {
   var parent = event.target.parentElement;
-  var listMenu = parent.getElementsByClassName('item-list')[0];
 
-  listMenu.classList.toggle('hide');
-  listMenu.classList.toggle('show');
+  if (parent.className === 'dropdown') {
+    var listMenu = parent.getElementsByClassName('item-list')[0];
 
-  /* if (listMenu.classList.contains('hide')) {
-    listMenu.classList.remove('hide');
-    listMenu.classList.add('show');
+    listMenu.classList.toggle('hide');
+    listMenu.classList.toggle('show');
+  }
+}
+
+var menuSedeCohort = document.querySelector('.item-list-sede-cohort-js');
+
+menuSedeCohort.addEventListener('click', selectCampusCohort);
+
+// logica para seleccionar dropdown
+function selectCampusCohort(event) {
+  if (event.target.className === 'line') {
+    var totalCenters = Object.keys(centers).length;
+    var centinel = false;
+    var campus = null;
+
+    for (var i = 0; i < totalCenters && !centinel; i++) {
+      if (centers[i].name === event.target.textContent.toLowerCase().trim()) {
+        centinel = true;
+        campus = centers[i];
+      }
+    }
+
+    var title = document.getElementsByClassName('cohort')[0].firstElementChild;
+    var cohort = Object.keys(data[campus.key])[0];
+
+    title.textContent = (campus.name + ' ' + cohort).toUpperCase();   
+    
+    callOverview(data, campus.key, cohort);
   } else {
-    listMenu.classList.remove('show');
-    listMenu.classList.add('hide');
-  } */
+    var centinel = false;
+    var sibling = event.target;
+
+    // se busca el hermano anterior para encontrar la sede
+    while (!centinel) {
+      sibling = sibling.previousElementSibling;
+
+      if (sibling.className === 'line') {
+        centinel = true;
+      }
+    }
+
+    var totalCenters = Object.keys(centers).length;
+    var campus = null;
+
+    centinel = false;
+
+    for (var i = 0; i < totalCenters && !centinel; i++) {
+      if (centers[i].name === sibling.textContent.toLowerCase().trim()) {
+        centinel = true;
+        campus = centers[i];
+      }
+    }
+
+    var title = document.getElementsByClassName('cohort')[0].firstElementChild;
+    var cohort = event.target.textContent.toLowerCase().trim();
+
+    title.textContent = (campus.name + ' ' + cohort).toUpperCase();   
+
+    callOverview(data, campus.key, cohort);
+  }
+
+  event.target.parentElement.classList.toggle('hide');
+  event.target.parentElement.classList.toggle('show');
+}
+
+// se llaman a los metodos que modifican el overview
+function callOverview(data, campus = campusDefault, cohort = cohortDefault) {
+  showEnrollment(data, campus, cohort);
+  showAchievement(data, campus, cohort);
+  showNetPrometerScore(data, campus, cohort);
+  showTechSkills(data, campus, cohort);
+  showStudentSatisfaction(data, campus, cohort);
+  showTeacherRating(data, campus, cohort);
+  showLastJediMasterRating(data, campus, cohort);
 }
